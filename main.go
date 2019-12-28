b22887b859fee24657c994afb0117d44df774eeb
karthin <24614028+karfy@users.noreply.github.com>
2019-12-28 10:17:16 -0500

    Support for numbers and supported symbols
    Kinda works for unsupported symbols (e.g. . , $) but somewhat broken and an index out of range panic could happen

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 			break
 		}
 
-		bigtext := bigify(scanner.Text())
+		bigtext := bigify(strings.ToLower(scanner.Text()))
 
 		clipboard.WriteAll(bigtext)
 
@@ -32,24 +32,55 @@ func main() {
 }
 
 func bigify(text string) string {
-	text = strings.TrimSpace(text)
 
-	r, _ := regexp.Compile("\\s+")
-	text = r.ReplaceAllString(text, " ")
+	// r, _ := regexp.Compile("\\s+")
+	text = regexp.MustCompile("[\\s]+").ReplaceAllString(text, " ")
 
 	letters := strings.Split(text, "")
 
 	var bigletters []string
 
-	for _, value := range letters {
-		if value != " " {
-			bigletters = append(bigletters, ":regional_indicator_"+strings.ToLower(value)+":")
+	for index, value := range letters {
+		if regexp.MustCompile("[a-zA-Z]").MatchString(value) {
+			value = ":regional_indicator_" + value + ":"
+		} else if regexp.MustCompile("[0-9]").MatchString(value) {
+			switch value {
+			case "0":
+				value = ":zero:"
+			case "1":
+				value = ":one:"
+			case "2":
+				value = ":two:"
+			case "3":
+				value = ":three:"
+			case "4":
+				value = ":four:"
+			case "5":
+				value = ":five:"
+			case "6":
+				value = ":six:"
+			case "7":
+				value = ":seven:"
+			case "8":
+				value = ":eight:"
+			case "9":
+				value = ":nine:"
+			case "10":
+				value = ":keycap_ten:"
+			}
+		} else if value == " " && letters[index+1] != "$" {
+		} else if value == "#" {
+			value = ":hash:"
+		} else if value == "?" {
+			value = ":question:"
 		} else {
-			bigletters = append(bigletters, " ")
+			bigletters = append(bigletters, value)
+			continue
 		}
+		bigletters = append(bigletters, value+" ")
 	}
 	// fmt.Println("----------------- Here ya go -----------------")
 	// fmt.Println(strings.Join(bigletters, " "))
 	// fmt.Println("----------------------------------------------")
-	return strings.Join(bigletters, " ")
+	return strings.Join(bigletters, "")
 }
