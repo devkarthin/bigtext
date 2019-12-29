aebb655fadc905f9d9c4f54c842666fef12b435b
karthin <24614028+karfy@users.noreply.github.com>
2019-12-29 17:08:16 -0500

    Rewrote all of the code
    Rewrote all of the code to make the program work on more than just discord

--- main.go
+++ main.go
@@ -13,76 +13,40 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("----------------- Welcome to discord big text utility by karthin#0073 -----------------")
-
 	for {
-
 		fmt.Print("Enter text: ")
 		if !scanner.Scan() {
 			break
 		}
 
-		bigtext := bigify(strings.ToLower(scanner.Text()))
+		bigtext, err := bigify(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
 
-		clipboard.WriteAll(bigtext)
-
-		fmt.Println("----------------- Text copied! -----------------")
+		if err := clipboard.WriteAll(bigtext); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Text copied")
 
 	}
 }
 
-func bigify(text string) string {
+func bigify(text string) (string, error) {
+	r, err := regexp.Compile("[\\s]+")
+	if err != nil {
+		return "", err
+	}
 
-	// r, _ := regexp.Compile("\\s+")
-	text = regexp.MustCompile("[\\s]+").ReplaceAllString(text, " ")
-
+	text = r.ReplaceAllString(text, " ")
 	letters := strings.Split(text, "")
-
 	var bigletters []string
 
-	for _, value := range letters {
-		if regexp.MustCompile("[a-zA-Z]").MatchString(value) {
-			value = ":regional_indicator_" + value + ":"
-		} else if regexp.MustCompile("[0-9]").MatchString(value) {
-			switch value {
-			case "0":
-				value = ":zero:"
-			case "1":
-				value = ":one:"
-			case "2":
-				value = ":two:"
-			case "3":
-				value = ":three:"
-			case "4":
-				value = ":four:"
-			case "5":
-				value = ":five:"
-			case "6":
-				value = ":six:"
-			case "7":
-				value = ":seven:"
-			case "8":
-				value = ":eight:"
-			case "9":
-				value = ":nine:"
-			case "10":
-				value = ":keycap_ten:"
-			}
-		} else if value == " " {
-		} else if value == "#" {
-			value = ":hash:"
-		} else if value == "?" {
-			value = ":question:"
-		} else if value == "!" {
-			value = ":exclamation:"
-		} else {
-			// bigletters = append(bigletters, value)
-			continue
-		}
-		bigletters = append(bigletters, value+" ")
+	for _, l := range letters {
+		emoji := getEmoji(l)
+		bigletters = append(bigletters, emoji)
+
 	}
-	// fmt.Println("----------------- Here ya go -----------------")
-	// fmt.Println(strings.Join(bigletters, " "))
-	// fmt.Println("----------------------------------------------")
-	return strings.Join(bigletters, "")
+	return strings.Join(bigletters, " "), nil
 }
